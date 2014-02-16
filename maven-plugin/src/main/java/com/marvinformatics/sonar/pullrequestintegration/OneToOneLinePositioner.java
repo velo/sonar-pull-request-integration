0f8f64ce5860288db03221bd41cf083b1f1a3e3b
Marvin Froeder <marvin@contaazul.com>
2014-02-16 19:07:26 -0300

    Fix some issues with nested projects

---
+++ maven-plugin/src/main/java/com/marvinformatics/sonar/pullrequestintegration/OneToOneLinePositioner.java
@@ -0,0 +1,9 @@
+package com.marvinformatics.sonar.pullrequestintegration;
+
+public class OneToOneLinePositioner implements LinePositioner {
+
+	public int toPostion(int line) {
+		return line;
+	}
+
+}
